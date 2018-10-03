e7fe3d72fb78dfb22b5412ebcdb97d679fda2072
Pietro Brenna <pietrobrenna@hotmail.it>
2018-10-03 19:02:21 +0200

    Implement leaf biased crossover (interpretation of)

--- src/gp/crossover.rs
+++ src/gp/crossover.rs
@@ -1,6 +1,6 @@
 use gp::*;
-use std::mem;
 use rand::Rng;
+use std::mem;
 
 /// The crossover mode in use. See `Crossover`.
 #[derive(PartialEq, Clone, Copy, Debug)]
@@ -23,7 +23,9 @@ impl Crossover {
     /// The subtree at a random position in one individual will be swapped with a random
     /// position in a second individual.
     pub fn one_point() -> Crossover {
-        Crossover { mode: CrossoverMode::OnePoint }
+        Crossover {
+            mode: CrossoverMode::OnePoint,
+        }
     }
 
     /// Get an operator to perform one-point crossover between two individuals.
@@ -33,13 +35,16 @@ impl Crossover {
     /// probability.
     #[doc(hidden)]
     pub fn one_point_leaf_biased(termpb: f32) -> Crossover {
-        Crossover { mode: CrossoverMode::OnePointLeafBiased(termpb) }
+        Crossover {
+            mode: CrossoverMode::OnePointLeafBiased(termpb),
+        }
     }
 
     /// Crossover (mate) two individuals according to the configured crossover mode.
     pub fn mate<T, R>(&self, indv1: &mut Individual<T>, indv2: &mut Individual<T>, rng: &mut R)
-        where T: Tree,
-              R: Rng
+    where
+        T: Tree,
+        R: Rng,
     {
         match self.mode {
             CrossoverMode::OnePoint => self.mate_one_point(indv1, indv2, rng),
@@ -49,40 +54,75 @@ impl Crossover {
         }
     }
 
-    fn mate_one_point<T: Tree, R: Rng>(&self,
-                                       indv1: &mut Individual<T>,
-                                       indv2: &mut Individual<T>,
-                                       rng: &mut R)
-        where T: Tree,
-              R: Rng
+    fn mate_one_point<T: Tree, R: Rng>(
+        &self,
+        indv1: &mut Individual<T>,
+        indv2: &mut Individual<T>,
+        rng: &mut R,
+    ) where
+        T: Tree,
+        R: Rng,
     {
         let target_index1 = rng.gen_range(0, indv1.nodes_count());
         let target_index2 = rng.gen_range(0, indv2.nodes_count());
 
-        indv1.tree.map_while(|node1, index1, _| if index1 == target_index1 {
-            indv2.tree.map_while(|node2, index2, _| if index2 == target_index2 {
-                mem::swap(node1, node2);
+        indv1.tree.map_while(|node1, index1, _| {
+            if index1 == target_index1 {
+                indv2.tree.map_while(|node2, index2, _| {
+                    if index2 == target_index2 {
+                        mem::swap(node1, node2);
+                        false
+                    } else {
+                        true
+                    }
+                });
                 false
             } else {
                 true
-            });
-            false
-        } else {
-            true
+            }
         });
 
         indv1.recalculate_metadata();
         indv2.recalculate_metadata();
     }
 
-    fn mate_one_point_leaf_biased<T, R>(&self,
-                                        _: &mut Individual<T>,
-                                        _: &mut Individual<T>,
-                                        _: f32,
-                                        _: R)
-        where T: Tree,
-              R: Rng
+    fn mate_one_point_leaf_biased<T, R>(
+        &self,
+        indv1: &mut Individual<T>,
+        indv2: &mut Individual<T>,
+        bias: f32,
+        rng: &mut R,
+    ) where
+        T: Tree,
+        R: Rng,
     {
-        unimplemented!();
+        let leaf = rng.gen_bool(f64::from(bias));
+
+        let target_index1 = rng.gen_range(0, indv1.nodes_count());
+        let mut target_index2 = rng.gen_range(0, indv2.nodes_count());
+        let mut node_counter = 0;
+        indv1.tree.map_while(|node1, index1, _| {
+            if index1 == target_index1 {
+                indv2.tree.map_while(|node2, _, _| {
+                    let is_leaf = node2.count_children() == 0;
+                    if is_leaf != leaf {
+                        target_index2 -= 1;
+                        true
+                    } else {
+                        let ret = if node_counter == target_index2 {
+                            mem::swap(node1, node2);
+                            false
+                        } else {
+                            true
+                        };
+                        node_counter += 1;
+                        ret
+                    }
+                });
+                false
+            } else {
+                true
+            }
+        });
     }
 }
